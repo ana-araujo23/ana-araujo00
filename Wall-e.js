371ac0e0b8d94cf2755e1bd602cb4d1fac22c9a1
ana-araujo23 <129509074+ana-araujo23@users.noreply.github.com>
2023-06-06 18:05:14 -0300

    Add files via upload
    Projeto falas para robô wall-e mandroid

---
+++ Wall-e.js
@@ -0,0 +1,69 @@
+const j = document.getElementById("b1");
+
+j.addEventListener('click', function () {
+    const audi = document.getElementById("a1")
+    audi.play();
+});
+
+const a = document.getElementById("b2");
+
+a.addEventListener('click', function () {
+    const audioo = document.getElementById("a2")
+    audioo.play();
+});
+
+const b = document.getElementById("b3");
+
+b.addEventListener('click', function () {
+    const audii = document.getElementById("a3")
+    audii.play();
+});
+
+const e = document.getElementById("b4");
+
+e.addEventListener('click', function () {
+    const audiio = document.getElementById("a4")
+    audiio.play();
+});
+
+const ju = document.getElementById("b5");
+
+ju.addEventListener('click', function () {
+    const audiiio = document.getElementById("a5")
+    audiiio.play();
+});
+
+const t = document.getElementById("b6");
+
+t.addEventListener('click', function () {
+    const audiiioo = document.getElementById("a6")
+    audiiioo.play();
+});
+
+const ca = document.getElementById("b7");
+
+ca.addEventListener('click', function () {
+    const audiiioy = document.getElementById("a7")
+    audiiioy.play();
+});
+
+const ba = document.getElementById("b8");
+
+ba.addEventListener('click', function () {
+    const audy = document.getElementById("a8")
+    audy.play();
+});
+
+const pi = document.getElementById("b9");
+
+pi.addEventListener('click', function () {
+    const audiioy = document.getElementById("a9")
+    audiioy.play();
+});
+
+const mi = document.getElementById("b10");
+
+mi.addEventListener('click', function () {
+    const audioy = document.getElementById("a10")
+    audioy.play();
+});
